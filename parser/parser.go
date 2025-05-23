143dba9b30b371f5d3bf82d7dd5f63e49c0f937c
divakaivan <timee98@outlook.com>
2025-05-23 20:43:19 +0900

    add an error field

--- parser/parser.go
+++ parser/parser.go
@@ -4,21 +4,36 @@ import (
 	"divakaivan/lang-interpreter-go/ast"
 	"divakaivan/lang-interpreter-go/lexer"
 	"divakaivan/lang-interpreter-go/token"
+	"fmt"
 )
 
 type Parser struct {
 	l *lexer.Lexer
 
+	errors    []string
 	curToken  token.Token
 	peekToken token.Token
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 	p.nextToken()
 	p.nextToken()
 
 	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
+		t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
 
 func (p *Parser) nextToken() {
