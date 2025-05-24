96a5d6abfd7d014b6a0597d30e9f46c17c2b1165
divakaivan <timee98@outlook.com>
2025-05-24 16:35:21 +0900

    update repl

--- repl/repl.go
+++ repl/repl.go
@@ -3,7 +3,7 @@ package repl
 import (
 	"bufio"
 	"divakaivan/lang-interpreter-go/lexer"
-	"divakaivan/lang-interpreter-go/token"
+	"divakaivan/lang-interpreter-go/parser"
 	"fmt"
 	"io"
 )
@@ -12,19 +12,29 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
-
 	for {
 		fmt.Print(PROMPT)
 		scanned := scanner.Scan()
 		if !scanned {
 			return
 		}
-
 		line := scanner.Text()
 		l := lexer.New(line)
-
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
