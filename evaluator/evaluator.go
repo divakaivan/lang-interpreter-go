f69ed4a24dab1b2571deee8c5b65b40447e535a3
divakaivan <timee98@outlook.com>
2025-05-28 12:44:06 +0900

    support string concat

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -165,6 +165,8 @@ func evalInfixExpression(
 	case left.Type() != right.Type():
 		return newError("type mismatch: %s %s %s",
 			left.Type(), operator, right.Type())
+	case left.Type() == object.STRING_OBJ && right.Type() == object.STRING_OBJ:
+		return evalStringInfixExpression(operator, left, right)
 	default:
 		return newError("unknown operator: %s %s %s",
 			left.Type(), operator, right.Type())
@@ -292,6 +294,19 @@ func evalExpressions(
 	}
 
 	return result
+}
+
+func evalStringInfixExpression(
+	operator string,
+	left, right object.Object,
+) object.Object {
+	if operator != "+" {
+		return newError("unknown operator: %s %s %s",
+			left.Type(), operator, right.Type())
+	}
+	leftVal := left.(*object.String).Value
+	rightVal := right.(*object.String).Value
+	return &object.String{Value: leftVal + rightVal}
 }
 
 func applyFunction(fn object.Object, args []object.Object) object.Object {
